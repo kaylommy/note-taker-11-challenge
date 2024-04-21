53066a025c34f0c064b3443c4498d222da73ae1d
kaylommy <kaylajfreeman13@gmail.com>
2024-04-21 18:54:15 -0400

    added content to README and fixed bug where when saving notes it would not display on page

--- Develop/server.js
+++ Develop/server.js
@@ -11,19 +11,16 @@ const app = express();
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 // Notes HTML route
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-// Homepage HTML route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 // Notes API get route
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (error, data) =>{
+    fs.readFile('./db/db.json', 'utf8', (error, data) =>{
         if (error) {
             throw error;
         }else {
@@ -35,21 +32,26 @@ app.get('/api/notes', (req, res) => {
 
 // Notes API post route
 app.post('/api/notes', (req, res) => {
-// TODO: Debug, fix JSON.parse (syntax error)
-    const notes = JSON.parse(notesDb)
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNotes = req.body;
+    // create a new id for every new note
     newNotes.id = uuidv4();
     notes.push(newNotes);
-    fs.writeFile('./db/db.json', JSON.stringify(notes))
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
     res.json(notes);
 })
 
-// Notes DELETE request
+// Notes DELETE request (bonus)
 app.delete('/api/notes/:id', (req, res) => {
 // TODO: add code for app.delete
 });
 
+// Homepage HTML route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 // listener for port
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
+    console.log(`listening on http://localhost:${PORT}`)
 });
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Note Taker Starter Code
 
+## Description
+This assignment was to create a note taker application with express.js.
+
+## Installation
+run 'npm i' in the console.
+run 'npm install uuid' in the console.
+
+## Usage
+run 'node server' in the console.
+
+## Screenshot
+
 ## Note
 The commit after 'first commit' contains most of my code because I made a mistake on pushing and accidentally pushed all my files and could not properly access my code. So I created a new repo.
